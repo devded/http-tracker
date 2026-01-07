89423d456ddec017036c247c55b2a66b97d35b70
Dedar <thededar@gmail.com>
2026-01-08 02:22:19 +0300

    feat: Implement capturing and displaying of HTTP response bodies with JSON beautification and truncation

--- src/js/httpTrackerDomEvents.js
+++ src/js/httpTrackerDomEvents.js
@@ -10,92 +10,94 @@ const errorHeaders = ['extraHeaders'];
 const r = httpTracker.browser.webRequest;
 
 r.onBeforeRequest.addListener(
-    function(details) {
-      details.callerName = 'onBeforeRequest';
-      details.requestIdEnhanced = details.requestId;
-      eventTracker.logRequestDetails(details);
-    }, trackUrls, reqBodyHeaders,
+  function (details) {
+    details.callerName = 'onBeforeRequest';
+    details.requestIdEnhanced = details.requestId;
+    eventTracker.logRequestDetails(details);
+  }, trackUrls, reqBodyHeaders,
 );
 
 r.onBeforeSendHeaders.addListener(
-    function(details) {
-      details.callerName = 'onBeforeSendHeaders';
-      details.requestIdEnhanced = details.requestId;
-      addModifyRequestHeaders(details);
-      eventTracker.logRequestDetails(details);
-      if (blockRequests(details)) {
-        return {
-          cancel: true,
-        };
-      }
+  function (details) {
+    details.callerName = 'onBeforeSendHeaders';
+    details.requestIdEnhanced = details.requestId;
+    addModifyRequestHeaders(details);
+    eventTracker.logRequestDetails(details);
+    if (blockRequests(details)) {
       return {
-        requestHeaders: details.requestHeaders,
+        cancel: true,
       };
-    }, trackUrls, reqHeadersBlocking,
+    }
+    return {
+      requestHeaders: details.requestHeaders,
+    };
+  }, trackUrls, reqHeadersBlocking,
 );
 
 r.onSendHeaders.addListener(
-    function(details) {
-      details.callerName = 'onSendHeaders';
-      details.requestIdEnhanced = details.requestId;
-      eventTracker.logRequestDetails(details);
-    }, trackUrls, reqHeaders,
+  function (details) {
+    details.callerName = 'onSendHeaders';
+    details.requestIdEnhanced = details.requestId;
+    eventTracker.logRequestDetails(details);
+  }, trackUrls, reqHeaders,
 );
 
 r.onHeadersReceived.addListener(
-    function(details) {
-      details.callerName = 'onHeadersReceived';
-      details.requestIdEnhanced = details.requestId;
-      eventTracker.logRequestDetails(details);
-    }, trackUrls, resHeaders,
+  function (details) {
+    details.callerName = 'onHeadersReceived';
+    details.requestIdEnhanced = details.requestId;
+    eventTracker.logRequestDetails(details);
+  }, trackUrls, resHeaders,
 );
 
 r.onAuthRequired.addListener(
-    function(details) {
-      details.callerName = 'onAuthRequired';
-      details.requestIdEnhanced = details.requestId;
-      eventTracker.logRequestDetails(details);
-    }, trackUrls, resHeaders,
+  function (details) {
+    details.callerName = 'onAuthRequired';
+    details.requestIdEnhanced = details.requestId;
+    eventTracker.logRequestDetails(details);
+  }, trackUrls, resHeaders,
 );
 
 r.onBeforeRedirect.addListener(
-    function(details) {
-      details.callerName = 'onBeforeRedirect';
-      details.requestIdEnhanced = details.requestId;
-      eventTracker.logRequestDetails(details);
-    }, trackUrls, resHeaders,
+  function (details) {
+    details.callerName = 'onBeforeRedirect';
+    details.requestIdEnhanced = details.requestId;
+    eventTracker.logRequestDetails(details);
+  }, trackUrls, resHeaders,
 );
 
 r.onResponseStarted.addListener(
-    function(details) {
-      details.callerName = 'onResponseStarted';
-      details.requestIdEnhanced = details.requestId;
-      eventTracker.logRequestDetails(details);
-    }, trackUrls, resHeaders,
+  function (details) {
+    details.callerName = 'onResponseStarted';
+    details.requestIdEnhanced = details.requestId;
+    eventTracker.logRequestDetails(details);
+  }, trackUrls, resHeaders,
 );
 
 r.onCompleted.addListener(
-    function(details) {
-      details.callerName = 'onCompleted';
-      details.requestIdEnhanced = details.requestId;
-      eventTracker.logRequestDetails(details);
-    }, trackUrls, resHeaders,
+  function (details) {
+    details.callerName = 'onCompleted';
+    details.requestIdEnhanced = details.requestId;
+    eventTracker.logRequestDetails(details);
+  }, trackUrls, resHeaders,
 );
 
 if (httpTracker.isFF) {
   r.onErrorOccurred.addListener(
-      function(details) {
-        details.callerName = 'onErrorOccurred';
-        details.requestIdEnhanced = details.requestId;
-        eventTracker.logRequestDetails(details);
-      }, trackUrls,
+    function (details) {
+      details.callerName = 'onErrorOccurred';
+      details.requestIdEnhanced = details.requestId;
+      eventTracker.logRequestDetails(details);
+    }, trackUrls,
   );
 } else {
   r.onErrorOccurred.addListener(
-      function(details) {
-        details.callerName = 'onErrorOccurred';
-        details.requestIdEnhanced = details.requestId;
-        eventTracker.logRequestDetails(details);
-      }, trackUrls, errorHeaders,
+    function (details) {
+      details.callerName = 'onErrorOccurred';
+      details.requestIdEnhanced = details.requestId;
+      eventTracker.logRequestDetails(details);
+    }, trackUrls, errorHeaders,
   );
 }
+
+debuggerTracker.init();
